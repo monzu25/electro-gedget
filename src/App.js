1e196722053ebc0bc18e2c41257ae088033f6bcb
monzu25 <monzu.ctg25@gmail.com>
2022-01-06 15:51:38 +0600

    Login Page setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component.jsx";
 import ShopPage from "./pages/shop/shop.component";
-
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-  useRouteMatch,
-  useParams,
-} from "react-router-dom";
+import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/signin-and-signup/signin-and-signup.component";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Router>
+        <Header />
         <Routes>
           <Route exact path="/" element={<HomePage />} />
           <Route path="/shop" element={<ShopPage />} />
+          <Route path="/signin" element={<SignInAndSignUpPage />} />
         </Routes>
       </Router>
     </div>
